b514a80cb03a719ce8eba0ba24ce431f5930cc3f
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2026-07-08 21:33:03 -0500

    add experimental stdlib file

---
+++ stdlib.lisp
@@ -0,0 +1,64 @@
+;; ==========================================
+;; 🦅 EAGLE-LISP STANDARD LIBRARY (stdlib.lisp)
+;; ==========================================
+
+;; --- STRING UTILITIES ---
+
+;; Reverses a string using pure Lisp recursion!
+(def string-reverse
+  (lambda (s)
+    (let ((len (string-length s)))
+      (if (<= len 1)
+          s
+          (string-append 
+            (string-reverse (substring s 1 len)) 
+            (substring s 0 1))))))
+
+(def string-append
+  (lambda (s1 s2)
+    (string-join "" (list s1 s2))))
+
+(def palindrome?
+  (lambda (s)
+    (= s (string-reverse s))))
+
+
+;; --- DATA STRUCTURES: STACK ---
+;; Stacks are easy because 'cons' prepends to the front of our lists!
+(def stack-new (lambda () (list)))
+(def stack-push (lambda (s item) (cons item s)))
+(def stack-pop (lambda (s) (car s)))
+(def stack-rest (lambda (s) (cdr s)))
+
+
+;; --- DATA STRUCTURES: QUEUE ---
+;; Queues are FIFO (First In, First Out). We append to the back, and pop from the front.
+(def queue-new (lambda () (list)))
+
+(def queue-enqueue
+  (lambda (q item)
+    (list-append q item)))
+
+(def queue-dequeue
+  (lambda (q)
+    (car q))) ;; Returns the oldest item
+
+(def queue-rest
+  (lambda (q)
+    (cdr q))) ;; Returns the queue without the oldest item
+
+
+;; --- MATH UTILITIES ---
+(def abs
+  (lambda (n)
+    (if (< n 0) (- n) n)))
+
+(def max
+  (lambda (a b)
+    (if (> a b) a b)))
+
+(def min
+  (lambda (a b)
+    (if (< a b) a b)))
+
+(puts "🦅 Standard Library loaded successfully!")
